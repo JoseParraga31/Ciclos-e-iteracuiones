593044ac0f1933af555d04f733a0ef115572f821
JoseParraga <joseparraga18@gmail.com>
2022-06-05 21:04:28 -0300

    Calculadora de años con mundial

--- Js/main.js
+++ Js/main.js
@@ -1,37 +1,8 @@
-//maquina expendedora de gaseosas 
-let ingreso = prompt(
-    "ingrese una opcion \n 1-CocaCola sabor original 350ml \n 2-Cocacola sin azucar 350ml \n 3-Cocacola black 350ml \n 4-fanta sabaor original 350ml \n 5-fanta sabaor sin azucar 350ml \n 6-agua con gas 350ml \n 7-agua sin gas 350ml \n 8-SevenUp sabor original 350ml \n 9-SevenUp sin azucar 350ml"
-);
+//ciclos e iteraciones
+    //calculando los años con mundial hasta el 2050
+    for (let AñoConMundial = 1930; AñoConMundial <= 2050; AñoConMundial = AñoConMundial + 4){
+        console.log("Este año se jugo un Mundial"+" "+ AñoConMundial);
+    }
 
-switch (ingreso) {
-    case "1":
-        alert("Selecionaste CocaCola sabor original 350ml, que lo disfrutes");
-        breack;
-    case "2":
-        alert("Selecionaste CocaCola sin azucar 350ml, que lo disfrutes");
-        breack;
-    case "3":
-        alert("Selecionaste Cocacola black 350ml, que lo disfrutes");
-        breack;
-    case "4":
-        alert("Selecionaste fanta sabaor original 350ml, que lo disfrutes");
-        breack; 
-    case "5":
-        alert("Selecionaste fanta sabaor sin azucar 350ml, que lo disfrutes");
-        breack;
-    case "6":
-        alert("Selecionaste Agua con gas 350ml, que lo disfrutes");
-        breack;
-    case "7":
-        alert("Selecionaste Agua sin gas 350ml, que lo disfrutes");
-        breack; 
-    case "8":
-        alert("Selecionaste SevenUp sabor original 350ml, que lo disfrutes");
-        breack;
-    case "9":
-        alert("Selecionaste SevenUp sin azucar 350ml, que lo disfrutes");
-        breack;
-     default:
-         alert("Opcion no valida");
-         break;                               
-}
+
+    
